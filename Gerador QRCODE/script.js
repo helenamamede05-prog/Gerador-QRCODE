23f5a7f62635d0d42fd851dbc3bc5a28f1907a7d
helenamamede05-prog <helena.mamede05@gmail.com>
2026-06-10 21:55:29 -0300

    Add files via upload

---
+++ Gerador QRCODE/script.js
@@ -0,0 +1,40 @@
+const box = document.querySelector("#box");
+const divQrCode = document.querySelector(".right");
+
+function gerarCodigoQr() {
+
+    let campoQr = document.querySelector("#campo-qr").value;
+
+    box.innerHTML = "";
+
+    if (campoQr.trim() !== "") {
+
+        new QRCode(box, {
+            text: campoQr,
+            width: 250,
+            height: 250
+        });
+
+        setTimeout(() => {
+
+            const img = box.querySelector("img");
+
+            if(img){
+
+                document
+                    .querySelector("#download")
+                    .href = img.src;
+
+                const logo = document.createElement("img");
+
+                box.appendChild(logo);
+            }
+
+        }, 300);
+
+    } else {
+
+        alert("Campo vazio!");
+
+    }
+}
